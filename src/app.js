ee9c32026f7a157f00fe716c800870081ba6931d
annakurylo <anna.k.2003.g@gmail.com>
2023-08-13 15:15:36 +0300

    ADD: multi price request

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const API_KEY =
   "c96718b4c204953b6dd304ddbebbaa9c9398ec61b0c530a25d5a1ac4237e9563";
 
-const CRYPTOCOMPARE_URL = "https://min-api.cryptocompare.com/data/price";
+const CRYPTOCOMPARE_URL = "https://min-api.cryptocompare.com/data/pricemulti";
 
 export const getPricesOfTickers = (tickers) => {
   return fetch(
     CRYPTOCOMPARE_URL +
       "?" +
       new URLSearchParams({
-        fsym: "USD",
-        tsyms: tickers.join(","),
+        fsyms: tickers.join(","),
+        tsyms: "USD",
         api_key: API_KEY,
       })
   )
@@ -18,7 +18,7 @@ export const getPricesOfTickers = (tickers) => {
       Object.fromEntries(
         Object.entries(cryptocurrencies).map((currency) => [
           currency[0],
-          1 / currency[1],
+          currency[1].USD,
         ])
       )
     );
